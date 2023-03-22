7f2fb51b30568310c9465d3bee348cc8714af300
Natália <116600900+rosbaki@users.noreply.github.com>
2023-03-22 20:39:53 -0300

    Update CalculadoraIMC.java

--- src/calculadoraimc/CalculadoraIMC.java
+++ src/calculadoraimc/CalculadoraIMC.java
@@ -57,11 +57,11 @@ public class CalculadoraIMC {
     public String classificarIMC() {
         if (calcularIMC() < 18.5)
             return "Abaixo do peso";
-        else if (calcularIMC() >= 18.5 || calcularIMC() <= 24.9)
+        else if (calcularIMC() >= 18.5 && calcularIMC() <= 24.9)
             return "Peso normal";
-        else if (calcularIMC() >= 25.0 || calcularIMC() <= 29.9)
+        else if (calcularIMC() >= 25.0 && calcularIMC() <= 29.9)
             return "Sobrepeso";
-        else if (calcularIMC() >= 30.0 || calcularIMC() <= 39.9)
+        else if (calcularIMC() >= 30.0 && calcularIMC() <= 39.9)
             return "Obesidade";
         else if (calcularIMC() >= 40.0)
             return "Obesidade grave";
